525c91c0b351ba8558360318742e1e517c93ffba
Akhilesh <akhileshmasali488@gmail.com>
2026-09-02 21:45:15 +0530

    tool creation: clone_repo

---
+++ tools/clone_repo.py
@@ -0,0 +1,17 @@
+from git import Repo, GitCommandError
+from pathlib import Path
+
+def clone_repo(repo_url: str, clone_dir: str = "./target_repo") -> dict:
+
+    try:
+        target_repo = Path(clone_dir)
+
+        if target_repo.exists() and any(target_repo.iterdir()):
+            return {"success": False, "path": None, "error": f"repository {target_repo} isn't empty"}
+
+        Repo.clone_from(repo_url, clone_dir)
+        return {"success": True, "path": clone_dir, "error": None}
+
+    except GitCommandError as e:
+        return {"success": False, "path": None, "error": str(e)}
+
